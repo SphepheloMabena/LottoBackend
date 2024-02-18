382cfb4fd91ab5339145925fbfd7c75460a8930f
Sphephelo Mabena <sphephelomabena@gmail.com>
2024-02-18 13:37:24 +0200

    Created SQL Lite Database

---
+++ db/Database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLACLHEMY_DATABASE_URL = 'sqlite:///.lottery.db'
+
+engine = create_engine(SQLACLHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+SessionLocal = sessionmaker(autocmmit= False, autoflush=False, bind=engine)
+
+Base = declarative_base()
