c2bae6c4948e66dbab5f1ac2894daae0f16e8cff
Sphephelo Mabena <sphephelomabena@gmail.com>
2024-02-18 14:50:57 +0200

    Created User Table

---
+++ db/Tables/Users.py
@@ -0,0 +1,11 @@
+from ..Database import Base
+from sqlalchemy import Column, Integer, String
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    hashed_password = Column(String)
+    role = Column(String)
