658cc25409cd156439f8329a39ecbed9d3c7b6dd
Sphephelo Mabena <sphephelomabena@gmail.com>
2024-03-21 12:56:31 +0200

    Moving to PostGreSql

--- db/Database.py
+++ db/Database.py
@@ -2,9 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLACLHEMY_DATABASE_URL = 'sqlite:///./lottery.db'
 
-engine = create_engine(SQLACLHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+SQLACLHEMY_DATABASE_URL = 'postgresql://koyeb-adm:P1k9ehfYAOul@ep-weathered-cloud-a2w0wacj.eu-central-1.pg.koyeb.app/koyebdb'
+
+engine = create_engine(SQLACLHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
